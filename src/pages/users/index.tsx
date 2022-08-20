641fdf3e32e6bb3bbcff62d426228a836e30b688
Guilherme Magno <guilhermertfmagno@gmail.com>
2022-08-20 19:04:21 -0300

    chore: config mirage fake API with faker and react query on users page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
     Flex,
     Heading,
     Icon,
+    Spinner,
     Table,
     Tbody,
     Td,
@@ -16,11 +17,42 @@ import {
 } from '@chakra-ui/react'
 import Link from 'next/link'
 import { RiAddLine } from 'react-icons/ri'
+import { useQuery } from 'react-query'
+
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 
 export default function UsersList() {
+    const { data, isLoading, error } = useQuery(
+        'users',
+        async () => {
+            const response = await fetch('http://localhost:3000/api/users')
+            const data = await response.json()
+
+            const users = data.users.map((user) => {
+                return {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    createdAt: new Date(user.createdAt).toLocaleDateString(
+                        'pt-BR',
+                        {
+                            day: '2-digit',
+                            month: 'long',
+                            year: 'numeric',
+                        }
+                    ),
+                }
+            })
+
+            return users
+        },
+        {
+            staleTime: 1000 * 5, // five seconds
+        }
+    )
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -63,41 +95,62 @@ export default function UsersList() {
                         </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th
-                                    paddingX={['4', '4', '6']}
-                                    color="gray.300"
-                                    width="8"
-                                >
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                {isWideVersion && <Th>Data de Cadastro</Th>}
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td paddingX={['4', '4', '6']}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">
-                                            Guilherme Magno
-                                        </Text>
-                                        <Text fontSize="sm" color="gray.300">
-                                            guilherme.magno@exemplo.com
-                                        </Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>15/06/2022</Td>}
-                            </Tr>
-                        </Tbody>
-                    </Table>
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>Falha ao obter dados dos usuários</Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th
+                                            paddingX={['4', '4', '6']}
+                                            color="gray.300"
+                                            width="8"
+                                        >
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && (
+                                            <Th>Data de Cadastro</Th>
+                                        )}
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.map((user) => (
+                                        <Tr key={user.id}>
+                                            <Td paddingX={['4', '4', '6']}>
+                                                <Checkbox colorScheme="pink" />
+                                            </Td>
+                                            <Td>
+                                                <Box>
+                                                    <Text fontWeight="bold">
+                                                        {user.name}
+                                                    </Text>
+                                                    <Text
+                                                        fontSize="sm"
+                                                        color="gray.300"
+                                                    >
+                                                        {user.email}
+                                                    </Text>
+                                                </Box>
+                                            </Td>
+                                            {isWideVersion && (
+                                                <Td>{user.createdAt}</Td>
+                                            )}
+                                        </Tr>
+                                    ))}
+                                </Tbody>
+                            </Table>
 
-                    <Pagination />
+                            <Pagination />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
